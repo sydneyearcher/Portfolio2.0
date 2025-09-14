1cf9bd02ccfa0e66cd5394325d7a4bfe35ebd7c5
sydneyearcher <123509734+sydneyearcher@users.noreply.github.com>
2025-09-14 19:49:24 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+//for slider
+
+let next = document.querySelector('.next')
+let prev = document.querySelector('.prev')
+
+next.addEventListener('click', function(){
+    let items = document.querySelectorAll('.item')
+    document.querySelector('.slide').appendChild(items[0])
+})
+
+prev.addEventListener('click', function(){
+    let items = document.querySelectorAll('.item')
+    document.querySelector('.slide').prepend(items[items.length - 1]) //here length of items is 6
+})
