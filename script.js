c67dbc8a9087565d48c12efb9fc7f1418d9f67b0
sydneyearcher <123509734+sydneyearcher@users.noreply.github.com>
2025-09-18 10:09:06 -0400

    Final Changes

--- script.js
+++ script.js
@@ -12,3 +12,19 @@ prev.addEventListener('click', function(){
     let items = document.querySelectorAll('.item')
     document.querySelector('.slide').prepend(items[items.length - 1]) //here length of items is 6
 })
+
+
+//for nav bar
+const toggleBtn = document.querySelector('.toggle_btn')
+const toggleBtnIcon = document.querySelector('.toggle_btn i')
+const dropDownMenu = document.querySelector('.dropdown_menu')
+
+toggleBtn.onclick = function () {
+    dropDownMenu.classList.toggle('open')
+    const isOpen = dropDownMenu.classList.contains('open')
+
+    toggleBtnIcon.className = isOpen
+        ? 'fa-solid fa-xmark'
+        : 'fa-solid fa-bars'
+}
+
